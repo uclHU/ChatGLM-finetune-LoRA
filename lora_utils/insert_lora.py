6fdff593b02f6e4837257cc0911c98a244da65f0
uclHU <115698600+uclHU@users.noreply.github.com>
2023-11-08 16:49:17 +0800

    Update insert_lora.py to add lora from loralib

--- lora_utils/insert_lora.py
+++ lora_utils/insert_lora.py
@@ -1,41 +1,42 @@
-# import numpy as np
-# import loralib as lora
+import numpy as np
+import loralib as lora
 
-# def get_lora_model_loralib(model, lora_config):
-    
-#     # lora_config = {
-#     #     'r': 32,
-#     #     'lora_alpha':32,
-#     #     'lora_dropout':0.1,
-#     #     'enable_lora':[True, True, True],
-#     # }
+def get_lora_model_loralib(model, lora_config):
 
-#     for key, module in model.named_modules():
-#         if key.endswith('attention'):
-#             if isinstance(module.query_key_value, lora.MergedLinear):
-#                 module.query_key_value.r = lora_config['r']
-#                 module.query_key_value.lora_alpha = lora_config['lora_alpha']
-#                 module.query_key_value.lora_dropout.p = lora_config['lora_dropout']
-#             else:
-#                 qkv_proj = lora.MergedLinear(module.query_key_value.in_features,
-#                                             3*module.query_key_value.in_features, 
-#                                             **lora_config,
-#                                             dtype=module.query_key_value.weight.data.dtype)
-#                 qkv_proj.load_state_dict(module.query_key_value.state_dict(), strict=False)
-#                 module.query_key_value = qkv_proj
+    if not lora_config:
+        lora_config = {
+            'r': 32,
+            'lora_alpha':32,
+            'lora_dropout':0.1,
+            'enable_lora':[True, True, True],
+        }
+
+    for key, module in model.named_modules():
+        if key.endswith('attention'):
+            if isinstance(module.query_key_value, lora.MergedLinear):
+                module.query_key_value.r = lora_config['r']
+                module.query_key_value.lora_alpha = lora_config['lora_alpha']
+                module.query_key_value.lora_dropout.p = lora_config['lora_dropout']
+            else:
+                qkv_proj = lora.MergedLinear(module.query_key_value.in_features,
+                                            3*module.query_key_value.in_features, 
+                                            **lora_config,
+                                            dtype=module.query_key_value.weight.data.dtype)
+                qkv_proj.load_state_dict(module.query_key_value.state_dict(), strict=False)
+                module.query_key_value = qkv_proj
 
 
-#     lora.mark_only_lora_as_trainable(model)
+    lora.mark_only_lora_as_trainable(model)
 
-#     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
-#     trainable_params = sum([np.prod(p.size()) for p in model_parameters])
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    trainable_params = sum([np.prod(p.size()) for p in model_parameters])
 
-#     model_parameters = filter(lambda p: not p.requires_grad, model.parameters())
-#     non_trainable_params = sum([np.prod(p.size()) for p in model_parameters])
+    model_parameters = filter(lambda p: not p.requires_grad, model.parameters())
+    non_trainable_params = sum([np.prod(p.size()) for p in model_parameters])
 
-#     print('trainable_params:{} ({:.2f}%), non_trainable_params:{}'.format(trainable_params, trainable_params/non_trainable_params*100,non_trainable_params))
+    print('trainable_params:{} ({:.2f}%), non_trainable_params:{}'.format(trainable_params, trainable_params/non_trainable_params*100,non_trainable_params))
 
-#     return model
+    return model
 
 ### loralib too slow
 
